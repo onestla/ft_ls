3bf5530fb0eb14394c16c0b1b70540da50674a73
Guillaume <glavigno@student.42.fr>
2018-12-10 20:09:25 +0100

    Draft

--- ft_ls.h
+++ ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: apeyret <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 19:04:14 by apeyret           #+#    #+#             */
-/*   Updated: 2018/12/10 19:31:00 by apeyret          ###   ########.fr       */
+/*   Updated: 2018/12/10 20:05:36 by glavigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,11 @@ typedef struct		s_path
 	char			*folder;
 	struct s_path	*next;
 }					t_path;
+
+typedef struct		s_info
+{
+
+	struct s_info	*next;
+}					t_info;				
 
 #endif
